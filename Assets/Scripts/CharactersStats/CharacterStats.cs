fd69d42d4ebb38d2662d25e60b83c1a856c2595d
Sergey Kashin <sergey.don.kashin@gmail.com>
2014-02-03 13:26:04 +0400

    Small update. Started implementatiion of Character's stats.

---
+++ Assets/Scripts/CharactersStats/CharacterStats.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStats : MonoBehaviour
+{
+//------------------------------------ HEALTH ------------------------------------------//
+    [SerializeField] private int health = 100;
+
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                value = 0;
+            }
+            health = value;
+        }
+    }
+
+    public void applyHealthChange(int value)
+    {
+        Health += value;
+    }
+
+//------------------------------------ MANA ------------------------------------------//
+    [SerializeField] private int mana = 100;
+    public int Mana
+    {
+        get
+        {
+            return mana;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                value = 0;
+            }
+            mana = value;
+        }
+    }
+
+    public void applyManaChange(int value)
+    {
+        Mana += value;
+    }
+}
